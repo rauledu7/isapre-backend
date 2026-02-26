e0f07ef2a3187a4693f38357d9652c3f7a9fafc5
rauledu7 <rfebrescorderor@gmail.com>
2026-02-25 21:47:47 -0300

    fix: add family ip in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,15 @@ import { ClientsModule } from './modules/clients/clients.module';
 
 @Module({
   imports: [
+    // 1. Cargamos las variables de entorno de Google Cloud Run
     ConfigModule.forRoot({ isGlobal: true }),
 
     TypeOrmModule.forRootAsync({
       useFactory: () => {
         /**
-         * 🛠️ PROCESAMIENTO DE CONEXIÓN
+         * 🛠️ PROCESAMIENTO DE CONEXIÓN (Blindaje Máximo)
          * Extraemos los datos de la URL para forzar IPv4 manualmente.
+         * Esto evita errores de red (como ENETUNREACH o EAI_AGAIN) en la nube.
          */
         const dbUrl = new URL(process.env.DATABASE_URL || '');
         
@@ -25,15 +27,16 @@ import { ClientsModule } from './modules/clients/clients.module';
           database: dbUrl.pathname.slice(1) || 'postgres',
           
           autoLoadEntities: true,
-          synchronize: true,
+          synchronize: true, // Crea las tablas en Supabase automáticamente
           logging: true,
           
+          // SSL es obligatorio para Supabase
           ssl: { rejectUnauthorized: false },
 
           /**
-           * 🚨 EL ÚLTIMO RECURSO
-           * Al pasar el host por separado y no como URL completa, 
-           * esta opción de 'family: 4' se vuelve obligatoria para el socket.
+           * 🚨 EL FILTRO DEFINITIVO
+           * Al pasar el host por separado, esta opción de 'family: 4' 
+           * obliga al socket de red a usar IPv4 exclusivamente.
            */
           extra: {
             family: 4,
